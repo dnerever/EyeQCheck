31090af32036d393f4e6d0cfddda5c08e61420f2
Keith Bates <bateskeith@pm.me>
2023-03-04 13:04:58 -0700

    complete image capture test

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import numpy as np
 
 cam_port = 0
 cam = VideoCapture(cam_port)
+
+result, image = cam.read()
+
+if results:
+    imshow("testImage", image)
+    imwrite("testImage.png", image)
+    waitKey(0)
+    destroyWindow("testImage")
+
+else:
+    print("No image. Try again")
